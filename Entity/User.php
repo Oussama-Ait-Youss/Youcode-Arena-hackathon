5bccc49c6dd6f43d8a8bf152c001d915c52b89ef
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-01-06 01:01:36 +0100

    I added user class that contains all properties we need and theirs generic getter"

--- Entity/User.php
+++ Entity/User.php
@@ -1,1 +1,41 @@
-    
+<?php   
+
+
+
+class User
+{
+    private int $id;
+    private string $username;
+    private string $email;
+    private string $password;
+    private string $role;
+    private ?string $avatar_url;
+    private ?string $bio;
+    private string $created_at;
+    const Admin = 'admin';
+    const Organizer = 'organizer';
+    const User = 'user';
+    public function __construct(
+        int $id,
+        string $username,
+        string $email,
+        string $password,
+        string $role,
+        ?string $avatar_url,
+        ?string $bio,
+        string $created_at
+    ) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+        $this->avatar_url = $avatar_url;
+        $this->bio = $bio;
+        $this->created_at = $created_at;
+    }
+    public function __get(string $property)
+    {
+        return $this->$property;
+    }
+}
