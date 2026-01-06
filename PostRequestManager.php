c7e31500d5e3e4c68681dabc982ff077680481ab
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-01-06 01:59:10 +0100

    this file is managin just post request

---
+++ PostRequestManager.php
@@ -0,0 +1,26 @@
+<?php
+include_once './Entity/User.php';
+include_once './Repository/UserRepo.php';
+
+
+
+if (isset($_POST["join"]) && $_POST["join"] === "signup") {
+    // Include necessary files
+
+
+    // Get form data
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = User::User; 
+   
+    $created_at = date('Y-m-d');
+
+    $user = new User($username, $email, $password, $role,$created_at);
+
+    $userRepo = new UserRepo();
+    $userRepo->Register($user);
+
+    header("Location: profile.php");
+    exit();
+}
